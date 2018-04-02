625aba8aee983e62bdbe516f48ed2258f6cf5a42
David Clark <dclark@greenchef.com>
2018-04-01 21:42:46 -0600

    Finished the game enough for me to be happy with it and what I learned.

--- src/App.vue
+++ src/App.vue
@@ -19,16 +19,16 @@
   </div>
   <div class="buttons-section main-section">
     <div class="new-game-buttons buttons-container" v-if="gameState === gameStates.new">
-      <button @click="gameState = gameStates.playing">Start New Game</button>
+      <button @click="startNewGame">Start New Game</button>
     </div>
     <div class="playing-game-buttons buttons-container" v-else>
-      <button>Attack</button>
-      <button>Heal</button>
-      <button @click="gameState = gameStates.new">Surrender</button>
+      <button @click="attack">Attack</button>
+      <button @click="heal">Heal</button>
+      <button @click="endGame">Surrender</button>
     </div>
   </div>
-  <div class="game-log-section main-section" v-if="logs.length">
-
+  <div class="game-log-section main-section" v-if="gameState === gameStates.playing && logs.length">
+    <div class="log" :class="[log.source.toLowerCase(), log.action.toLowerCase()]" v-for="log in reversedLogs">{{ log.message }}</div>
   </div>
 </div>
 
@@ -38,6 +38,36 @@
 <!-- SCRIPT -->
 
 <script>
+const SOURCES = {
+  player: 'Player',
+  monster: 'Monster',
+};
+
+const ACTIONS = {
+  attack: 'ATTACK',
+  heal: 'HEAL',
+};
+
+class GameLog {
+  constructor(source, action, value) {
+    this.source = source;
+    this.action = action;
+    this.value = value;
+
+    // set the message
+    switch (action) {
+      case ACTIONS.attack:
+        this.message = `The ${source} attacked for ${value}.`
+        break;
+      case ACTIONS.heal:
+        this.message = `The ${source} healed for ${value}.`
+        break;
+      default:
+        this.message = `Unknown action action: ${action}.`
+    }
+  }
+}
+
 const GAME_STATES = {
   new: 'NEW',
   playing: 'PLAYING',
@@ -55,9 +85,48 @@ export default {
       life: 100,
     },
     gameStates: GAME_STATES,
-    gameState: GAME_STATES.playing,
+    gameState: GAME_STATES.new,
     logs: [],
   }),
+  computed: {
+    reversedLogs() {
+      return [...this.logs].reverse();
+    },
+  },
+  methods: {
+    startNewGame() {
+      this.gameState = GAME_STATES.playing;
+      this.player.life = 100;
+      this.monster.life = 100;
+      this.logs = [];
+    },
+    endGame() {
+      this.gameState = GAME_STATES.new;
+    },
+    getMonsterAttack() { return Math.round(Math.random() * 10); },
+    getPlayerAttack() { return Math.round(Math.random() * 12); },
+    getPlayerHeal() { return Math.round(Math.random() * 15); },
+    attack() {
+      const playerAttack = this.getPlayerAttack();
+      const monsterAttack = this.getMonsterAttack();
+
+      this.player.life -= monsterAttack;
+      this.monster.life -= playerAttack;
+
+      this.logs.push(new GameLog(SOURCES.player, ACTIONS.attack, playerAttack));
+      this.logs.push(new GameLog(SOURCES.monster, ACTIONS.attack, monsterAttack));
+    },
+    heal() {
+      const playerHeal = this.getPlayerHeal();
+      const monsterAttack = this.getMonsterAttack();
+
+      this.player.life += playerHeal;
+      this.player.life -= monsterAttack;
+
+      this.logs.push(new GameLog(SOURCES.player, ACTIONS.heal, playerHeal));
+      this.logs.push(new GameLog(SOURCES.monster, ACTIONS.attack, monsterAttack));
+    },
+  },
 };
 </script>
 
@@ -115,6 +184,25 @@ export default {
 
 .buttons-container > button {
   margin: 10px;
+}
+
+.log {
+  padding: 5px;
+  margin: 5px;
+  background: lightgrey;
+  text-align: center;
+}
+
+.log.player {
+  background: lightblue;
+}
+
+.log.monster {
+  background: pink;
+}
+
+.log.heal {
+  background: lightgreen;
 }
 
 </style>
